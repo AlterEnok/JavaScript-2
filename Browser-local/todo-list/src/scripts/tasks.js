55cec1c8ef9e25c60cad9703dcb123fa1cbea85a
Edem Kestan <ed.kestan@gmail.com>
2023-10-15 18:45:53 +0300

    update

--- Browser-local/todo-list/src/scripts/tasks.js
+++ Browser-local/todo-list/src/scripts/tasks.js
@@ -1,8 +1,22 @@
 const LOCAL_STORAGE_KEY = 'tasksList';
 
+
+const loadTasksFromLocalStorage = () => {
+  const savedTasks = localStorage.getItem(LOCAL_STORAGE_KEY);
+  return savedTasks ? JSON.parse(savedTasks) : [];
+};
+
+
+const saveTasksToLocalStorage = () => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tasks));
+};
+
+
 const tasks = loadTasksFromLocalStorage();
 
+
 const getTasks = () => tasks;
+
 
 const addTask = (text) => {
   const newTask = {
@@ -11,29 +25,18 @@ const addTask = (text) => {
     done: false,
   };
   tasks.push(newTask);
+  saveTasksToLocalStorage(); 
   return newTask;
 };
+
 
 const updateTask = (taskId, isDone) => {
   const task = tasks.find((t) => t.id === taskId);
   if (task) {
     task.done = isDone;
+    saveTasksToLocalStorage(); 
   }
 };
 
+
 export { getTasks, addTask, updateTask };
-
-// Add the following lines at the end of the file
-
-function saveTasksToLocalStorage() {
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tasks));
-}
-
-function loadTasksFromLocalStorage() {
-  const savedTasks = localStorage.getItem(LOCAL_STORAGE_KEY);
-  return savedTasks ? JSON.parse(savedTasks) : [];
-}
-
-// Load tasks from localStorage on module load
-tasks.length = 0;
-tasks.push(...loadTasksFromLocalStorage());
