05adc9d0885d298e26287e97a19f396c42b846d6
Edem Kestan <ed.kestan@gmail.com>
2023-10-15 18:36:48 +0300

    update 3

--- Browser-local/todo-list/src/scripts/index.js
+++ Browser-local/todo-list/src/scripts/index.js
@@ -17,21 +17,35 @@
 // window.addEventListener('storage', onStorageChange);
 
 import { renderTasks } from './render';
-import { addTask } from './tasks';
+import { addTask, updateTask, getTasks } from './tasks';
 import { saveTasksToLocalStorage, loadTasksFromLocalStorage } from './storage';
 
 const createTaskBtn = document.querySelector('.create-task-btn');
 const newTaskText = document.querySelector('.task-input');
 
+const saveAndUpdateTasks = () => {
+  saveTasksToLocalStorage(getTasks());
+  renderTasks(getTasks());
+};
+
 createTaskBtn.addEventListener('click', () => {
   const taskText = newTaskText.value.trim();
   if (taskText) {
-    const newTask = addTask(taskText);
-    renderTasks(loadTasksFromLocalStorage());
-    saveTasksToLocalStorage(getTasks());
+    addTask(taskText);
+    saveAndUpdateTasks();
     newTaskText.value = '';
   }
 });
 
-
+// Load tasks from localStorage on page load
 renderTasks(loadTasksFromLocalStorage());
+
+// Update tasks in localStorage when checkbox state changes
+document.addEventListener('change', (evt) => {
+  if (evt.target.classList.contains('list__item-checkbox')) {
+    const taskId = +evt.target.dataset.id;
+    const isDone = evt.target.checked;
+    updateTask(taskId, isDone);
+    saveAndUpdateTasks();
+  }
+});
--- Browser-local/todo-list/src/scripts/tasks.js
+++ Browser-local/todo-list/src/scripts/tasks.js
@@ -1,28 +1,39 @@
-const tasks = [
-    { id: 1, text: 'Buy milk', done: false },
-    { id: 2, text: 'Pick up Tom from the airport', done: false },
-    { id: 3, text: 'Visit a party', done: false },
-    { id: 4, text: 'Visit a doctor', done: true },
-    { id: 5, text: 'Buy meat', done: true },
-  ];
-  
-  const getTasks = () => tasks;
-  
-  const addTask = (text) => {
-    const newTask = {
-      id: tasks.length + 1,
-      text,
-      done: false,
-    };
-    tasks.push(newTask);
-    return newTask;
+const LOCAL_STORAGE_KEY = 'tasksList';
+
+const tasks = loadTasksFromLocalStorage();
+
+const getTasks = () => tasks;
+
+const addTask = (text) => {
+  const newTask = {
+    id: tasks.length + 1,
+    text,
+    done: false,
   };
-  
-  const updateTask = (taskId, isDone) => {
-    const task = tasks.find((t) => t.id === taskId);
-    if (task) {
-      task.done = isDone;
-    }
-  };
-  
-  export { getTasks, addTask, updateTask };
+  tasks.push(newTask);
+  return newTask;
+};
+
+const updateTask = (taskId, isDone) => {
+  const task = tasks.find((t) => t.id === taskId);
+  if (task) {
+    task.done = isDone;
+  }
+};
+
+export { getTasks, addTask, updateTask };
+
+// Add the following lines at the end of the file
+
+function saveTasksToLocalStorage() {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tasks));
+}
+
+function loadTasksFromLocalStorage() {
+  const savedTasks = localStorage.getItem(LOCAL_STORAGE_KEY);
+  return savedTasks ? JSON.parse(savedTasks) : [];
+}
+
+// Load tasks from localStorage on module load
+tasks.length = 0;
+tasks.push(...loadTasksFromLocalStorage());
