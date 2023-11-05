19e90b7151cf106fdaf0b75e2d8c7048719a6038
Edem Kestan <ed.kestan@gmail.com>
2023-11-05 17:27:53 +0300

    2

--- Asynchronous-code/task1/index.js
+++ Asynchronous-code/task1/index.js
@@ -1,37 +1,37 @@
-export const addImage = (imgSrc, callback) => {
+const addImage = (imgSrc, callback) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My photo');
     imgElem.src = imgSrc;
+  
+    imgElem.addEventListener('load', () => {
+      const { width, height } = imgElem;
+      callback(null, imgElem);
+    });
+  
+    imgElem.addEventListener('error', () => {
+      callback('Image load failed');
+    });
+  
     const containerElem = document.querySelector('.page');
-    
+  
     if (containerElem) {
-        containerElem.append(imgElem);
-
-        imgElem.addEventListener('load', () => {
-            const { width, height } = imgElem;
-            callback(null, imgElem);
-        });
-
-        imgElem.addEventListener('error', () => {
-            containerElem.removeChild(imgElem);
-            callback(new Error('Image load failed')); 
-        });
+      containerElem.appendChild(imgElem);
     } else {
-        callback(new Error('Container not found')); 
+      callback('Container not found');
     }
-};
-
-const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-
-const onImageLoaded = (error, imgElem) => {
+  };
+  
+  const onImageLoaded = (error, imgElem) => {
     if (error) {
-        console.log(error.message); 
-        return;
+      console.log(error);
+      return;
     }
-
+  
     const { width, height } = imgElem;
     const sizeElem = document.querySelector('.image-size');
     sizeElem.textContent = `${width} x ${height}`;
-};
-
-addImage(imgSrc, onImageLoaded);
+  };
+  
+  const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+  
+  addImage(imgSrc, onImageLoaded);
