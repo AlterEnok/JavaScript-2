2e3ad768baa27e3037855f9299fe53f6b806dfa8
Edem Kestan <ed.kestan@gmail.com>
2023-11-05 15:39:09 +0300

    task1

--- Asynchronous-code/task1/index.js
+++ Asynchronous-code/task1/index.js
@@ -1,9 +1,37 @@
+// export const addImage = (imgSrc, callback) => {
+//     const imgElem = document.createElement('img');
+//     imgElem.setAttribute('alt', 'My photo');
+//     imgElem.src = imgSrc;
+//     const containerElem = document.querySelector('.page');
+//     containerElem.append(imgElem);
+
+//     imgElem.addEventListener('load', () => {
+//         const { width, height } = imgElem;
+//         callback(null, imgElem);
+//     });
+
+//     imgElem.addEventListener('error', () => callback('Image load failed'));
+// };
+
+// const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+// const onImageLoaded = (error, imgElem) => {
+//     if (error) {
+//         console.log(error);
+//         return;
+//     }
+
+//     const { width, height } = imgElem;
+//     const sizeElem = document.querySelector('.image-size');
+//     sizeElem.textContent = `${width} x ${height}`;
+// };
+
+// addImage(imgSrc, onImageLoaded);
+
 export const addImage = (imgSrc, callback) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My photo');
     imgElem.src = imgSrc;
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
 
     imgElem.addEventListener('load', () => {
         const { width, height } = imgElem;
@@ -11,6 +39,13 @@ export const addImage = (imgSrc, callback) => {
     });
 
     imgElem.addEventListener('error', () => callback('Image load failed'));
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const containerElem = document.querySelector('.page');
+        if (containerElem) {
+            containerElem.append(imgElem);
+        }
+    });
 };
 
 const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
