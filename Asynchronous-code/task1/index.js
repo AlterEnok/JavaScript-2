ac4cbbd06130e40c67b67931e6471fb321de4475
Edem Kestan <ed.kestan@gmail.com>
2023-11-05 16:56:27 +0300

    task1 update

--- Asynchronous-code/task1/index.js
+++ Asynchronous-code/task1/index.js
@@ -1,46 +1,9 @@
-
-// export const addImage = (imgSrc, callback) => {
-//     const imgElem = document.createElement('img');
-//     imgElem.setAttribute('alt', 'My photo');
-//     imgElem.src = imgSrc;
-
-//     imgElem.addEventListener('load', () => {
-//         const { width, height } = imgElem;
-//         callback(null, imgElem);
-//     });
-
-//     imgElem.addEventListener('error', () => callback('Image load failed'));
-
-//     document.addEventListener('DOMContentLoaded', () => {
-//         const containerElem = document.querySelector('.page');
-//         if (containerElem) {
-//             containerElem.append(imgElem);
-//         }
-//     });
-// };
-
-// const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-
-// const onImageLoaded = (error, imgElem) => {
-//     if (error) {
-//         console.log(error);
-//         return;
-//     }
-
-//     const { width, height } = imgElem;
-//     const sizeElem = document.querySelector('.image-size');
-//     sizeElem.textContent = `${width} x ${height}`;
-// };
-
-// addImage(imgSrc, onImageLoaded);
-
 export const addImage = (imgSrc, callback) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My photo');
     imgElem.src = imgSrc;
     const containerElem = document.querySelector('.page');
     
-   
     if (containerElem) {
         containerElem.append(imgElem);
 
@@ -49,9 +12,12 @@ export const addImage = (imgSrc, callback) => {
             callback(null, imgElem);
         });
 
-        imgElem.addEventListener('error', () => callback('Image load failed'));
+        imgElem.addEventListener('error', () => {
+            containerElem.removeChild(imgElem);
+            callback('Image load failed');
+        });
     } else {
-        callback('Container not found'); 
+        callback('Container not found');
     }
 };
 
