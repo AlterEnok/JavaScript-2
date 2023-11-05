180a29c6b72f3cdba939a5da1096c5bd98be7b4b
Edem Kestan <ed.kestan@gmail.com>
2023-11-05 15:15:15 +0300

    fix-task1

--- Asynchronous-code/task1/index.js
+++ Asynchronous-code/task1/index.js
@@ -5,34 +5,25 @@ export const addImage = (imgSrc, callback) => {
     const containerElem = document.querySelector('.page');
     containerElem.append(imgElem);
 
-    const onImageLoaded = () =>{
+    imgElem.addEventListener('load', () => {
         const { width, height } = imgElem;
-        callback(null, { width, height});
-    }
-
-    imgElem.addEventListener('load', onImageLoaded);
+        callback(null, imgElem);
+    });
 
     imgElem.addEventListener('error', () => callback('Image load failed'));
-
-    
-
-    
 };
 
 const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
-const onImageLoaded = (error, imgElem) =>{
-    if(error){
+const onImageLoaded = (error, imgElem) => {
+    if (error) {
         console.log(error);
         return;
     }
 
-    const {width,  height} = imgElem;
+    const { width, height } = imgElem;
     const sizeElem = document.querySelector('.image-size');
     sizeElem.textContent = `${width} x ${height}`;
 };
 
 addImage(imgSrc, onImageLoaded);
-
-
-
