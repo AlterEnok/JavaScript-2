8fb158e8b115716c644a257bd4a8f2ae0f03eed8
Edem Kestan <ed.kestan@gmail.com>
2023-11-05 16:52:49 +0300

    task1

--- Asynchronous-code/task1/index.js
+++ Asynchronous-code/task1/index.js
@@ -1,9 +1,8 @@
+
 // export const addImage = (imgSrc, callback) => {
 //     const imgElem = document.createElement('img');
 //     imgElem.setAttribute('alt', 'My photo');
 //     imgElem.src = imgSrc;
-//     const containerElem = document.querySelector('.page');
-//     containerElem.append(imgElem);
 
 //     imgElem.addEventListener('load', () => {
 //         const { width, height } = imgElem;
@@ -11,6 +10,13 @@
 //     });
 
 //     imgElem.addEventListener('error', () => callback('Image load failed'));
+
+//     document.addEventListener('DOMContentLoaded', () => {
+//         const containerElem = document.querySelector('.page');
+//         if (containerElem) {
+//             containerElem.append(imgElem);
+//         }
+//     });
 // };
 
 // const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
@@ -32,20 +38,21 @@ export const addImage = (imgSrc, callback) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My photo');
     imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    
+   
+    if (containerElem) {
+        containerElem.append(imgElem);
 
-    imgElem.addEventListener('load', () => {
-        const { width, height } = imgElem;
-        callback(null, imgElem);
-    });
+        imgElem.addEventListener('load', () => {
+            const { width, height } = imgElem;
+            callback(null, imgElem);
+        });
 
-    imgElem.addEventListener('error', () => callback('Image load failed'));
-
-    document.addEventListener('DOMContentLoaded', () => {
-        const containerElem = document.querySelector('.page');
-        if (containerElem) {
-            containerElem.append(imgElem);
-        }
-    });
+        imgElem.addEventListener('error', () => callback('Image load failed'));
+    } else {
+        callback('Container not found'); 
+    }
 };
 
 const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
